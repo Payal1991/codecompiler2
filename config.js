0382c63332cb7c2656451c15c477be9d6ff46c71
Payal1991 <1991payalsuchita@gmail.com>
2022-09-09 02:13:21 +0530

    Added All Files

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    clientId: "289d4ee9d08a16754819a84087d85456",
+    clientSecret:"ab0c200ee012982c1b7f2d1b31622165d7ab77275b52adcbdbc338bbe19cae42"
+}
---
+++ index.js
@@ -0,0 +1,38 @@
+// Library Imports
+const express = require("express");
+const bodyParser = require("body-parser")
+
+// Project Imports
+const routes = require("./routes")
+
+const PORT = process.env.PORT || 5000;
+const app = express()
+
+app.use(bodyParser.json())
+
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+app.use('/', routes) 
+
+// ----------deployment---------------------------
+
+if(process.env.NODE_ENV === "production"){
+    console.log(path.join(__dirname, "client", "build", "index.html"))
+    app.use(express.static(path.join(__dirname, "client", "build")))
+  
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    })
+  }
+  
+  // ----------deployment---------------------------
+
+app.listen(PORT, ()=>{
+console.log(`Server started at port ${PORT}`)
+})
